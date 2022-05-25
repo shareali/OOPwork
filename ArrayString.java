6a63091a274aea7f6d6d63fb0e8f9c712b728599
shar e ali <105540169+shareali@users.noreply.github.com>
2022-05-25 11:22:18 -0700

    Add files via upload

---
+++ ArrayString.java
@@ -0,0 +1,9 @@
+public class ArrayString
+{
+  public static void main(String args[])
+   {
+     String names[] = {"AbdullahAbid", "Huzaifa", "Sheraaz", "ShereAli"};
+      System.out.println(names.length);
+   }
+}
+ 
---
+++ Literals.java
@@ -0,0 +1,13 @@
+Public class Literals
+{
+  public static void main(String [] args)
+    {
+      int myInt=24;
+      float myFloat=2.5F;
+      long myLong=580_000L;
+      double myDouble=7.999d;
+      char myChar='C';
+
+      System.out.print("My Int= "+myInt+"\nMy Float= "+myFloat+"\nMy Long= "+myLong+"\nMy Double= "+myDouble+"\nMy Char= "+myChar);
+    }
+}
