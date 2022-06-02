31afb7d94d146b15431bb3060b2c9e0d807efaef
shar e ali <105540169+shareali@users.noreply.github.com>
2022-06-02 06:33:34 -0700

    Add files via upload

---
+++ new10 (1).java
@@ -0,0 +1,30 @@
+public class LIT
+{
+public static void main (string arg[])
+{
+long   b=562L;
+int    a=2;
+double d=6756.09;
+float  f=3.654f;
+int    hx=0x765;
+long   hxl=0x765L;
+float  hxf=0x12.2P2;
+int    oct=024;
+long   octl=024L;
+int     binary=0b1o1o1;
+boolean    bol=false;
+string   str="Name";
+System.Out.Pointln(b);
+System.Out.Pointln(a);
+System.Out.Pointln(d);
+System.Out.Pointln(f);
+System.Out.Pointln(hx);
+System.Out.Pointln(hxl);
+System.Out.Pointln(hxf);
+System.Out.Pointln(oct);
+System.Out.Pointln(octl);
+System.Out.Pointln(binary);
+System.Out.Pointln(bol);
+System.Out.Pointln(str);
+}
+}
---
+++ new8.java
@@ -0,0 +1,15 @@
+public class escapesequence
+{
+public static void main (string arg[])
+{
+System.Out.Pointln("My\nWorld");
+System.Out.Pointln("My\tWolrd");
+System.Out.Pointln("My\rWolrd");
+System.Out.Pointln("My\"Wolrd\"");
+System.Out.Pointln("My\\Wolrd\\");
+System.Out.Pointln("My\'Wolrd\'");
+System.Out.Pointln("octal unicode character: \141");
+System.Out.Pointln("hexadecimal unicode character: \u1418");
+
+}
+}
